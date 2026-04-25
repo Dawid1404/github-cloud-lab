2c96d60fd1f79f6c56828ec3c0a913301ae471ce
Dawid <d.aderek1404@gmail.com>
2026-04-25 20:59:57 +0000

    Dodaj hello.py z funkcją greet

---
+++ hello.py
@@ -0,0 +1,7 @@
+def greet(name):
+    """Zwraca powitanie."""
+    return f"Hello, {name}! Witaj w chmurze GitHub."
+
+if __name__ == "__main__":
+    print(greet("World"))
+    print(greet("GitHub"))
